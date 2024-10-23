89dc350268af97a623996aa7f9b190d16796f1df
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-10-23 13:26:33 +0300

    feat(meta tags): update meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Herkes için GNU/Linux ve Siber Güvenlik',
+    description: 'Topluluğumuz tarafından eğitimi verilen "Herkes için GNU/Linux ve Siber Güvenlik" kursunun dokümanlarını içerir',
+    image: 'public/images/pausiber-logo.png'
   }
 
   return (
